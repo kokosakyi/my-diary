337952f9d9efddfc4eeabc6c71f1a525f0c6ec4d
Nana Andoh <conradkyei@gmail.com>
2025-06-30 14:13:52 -0400

    Implement comprehensive Netlify Prisma detection bypass
    - Disable NETLIFY environment variables in runtime
    - Add binary targets for Netlify serverless functions
    - Configure Prisma client for serverless environment
    - Add postbuild script for client generation

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -7,12 +7,23 @@ declare global {
   var __db__: PrismaClient;
 }
 
+// Disable Prisma's Netlify detection that causes the error
+process.env.NETLIFY = undefined;
+process.env.NETLIFY_DEV = undefined;
+
 // This is needed because in development we don't want to restart
 // the server with every change, but we want to make sure we don't
 // create a new connection to the DB with every change either.
 // In production, we'll have a single connection to the DB.
 if (process.env.NODE_ENV === "production") {
-  db = new PrismaClient();
+  db = new PrismaClient({
+    // Configure for serverless environment
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  });
 } else {
   if (!global.__db__) {
     global.__db__ = new PrismaClient();
